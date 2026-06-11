72fef9500300dfa13b64797652e7ebc0319a3c32
ranxc <rxc99129@163.com>
2026-06-11 11:15:53 +0800

    两数之和

--- leetcode/editor/cn/TwoSum.java
+++ leetcode/editor/cn/TwoSum.java
@@ -63,13 +63,13 @@ public class TwoSum {
     class Solution {
         public int[] twoSum(int[] nums, int target) {
             // 暴力循环
-        /*for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
+            /*for (int i = 0; i < nums.length; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[]{i, j};
+                    }
                 }
-            }
-        }*/
+            }*/
             // 使用Map
             // 初始化哈希表，Key 为具体的数字，Value 为该数字在数组中的下标
             Map<Integer, Integer> map = new HashMap<>();
